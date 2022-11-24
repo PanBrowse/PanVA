066ba2042dac071ae027270c99843b7e04d8b915
Folkert de Vries <github@fdev.nl>
2022-11-24 16:45:39 +0100

    Locus view layout attempt

--- frontend/src/components/LocusView.vue
+++ frontend/src/components/LocusView.vue
@@ -1,15 +1,27 @@
 <script lang="ts">
-import { mapWritableState } from 'pinia'
+import { mapState, mapWritableState } from 'pinia'
 import { useDataStore } from '@/stores/data'
 import { range } from 'lodash'
+
+import { CELL_SIZE } from '@/config'
 
 export default {
   name: 'LocusView',
   computed: {
+    ...mapState(useDataStore, ['sequenceCount']),
+    // sequenceCount() {
+    //   return 10
+    // },
     ...mapWritableState(useDataStore, ['selectedRegion']),
     examples(): number[] {
       const [start, end] = this.selectedRegion
       return range(start, end + 1)
+    },
+    cellSize() {
+      return CELL_SIZE
+    },
+    contentHeight(): number {
+      return this.sequenceCount * this.cellSize
     },
   },
   methods: {
@@ -26,6 +38,7 @@ export default {
 <template>
   <a-card class="locus-view" title="Locus view" :bordered="false" size="small">
     <div class="header">
+      <!--
       <p>Selected region: {{ selectedRegion }}</p>
       <a-button type="primary" @click="resetSelectionOne">
         Reset selection to 100-150
@@ -33,10 +46,50 @@ export default {
       <a-button type="primary" @click="resetSelectionTwo">
         Reset selection to 60-64
       </a-button>
+      -->
+      <div style="height: 60px; background: red; flex: 0 0 150px">Dendro</div>
+      <div style="height: 60px; background: orange; flex: 0 0 100px">
+        Bipartite
+      </div>
+      <div style="height: 60px; background: yellow; overflow: auto">
+        <div
+          :style="{
+            width: examples.length * cellSize + 'px',
+            background: 'gold',
+          }"
+        >
+          Heatmap
+        </div>
+      </div>
+      <div style="height: 60px; background: lime; flex: 0 0 40px">Pheno 1</div>
+      <div style="height: 60px; background: green; flex: 0 0 120px">
+        Pheno 2
+      </div>
+      <div style="height: 60px; background: cyan; flex: 0 0 100px">Pheno 3</div>
     </div>
-    <div class="content">
-      <div class="long">
-        <p v-for="n in examples" v-bind:key="n">Card content line {{ n }}</p>
+
+    <div class="content-wrapper">
+      <div class="content" :style="{ height: contentHeight + 'px' }">
+        <div style="background: red; flex: 0 0 150px">Dendro</div>
+        <div style="background: orange; flex: 0 0 100px">Bipartite</div>
+        <div style="background: yellow; overflow-y: hidden; overflow-x: auto">
+          <div v-for="seq in sequenceCount" v-bind:key="seq" class="row">
+            <div
+              :style="{
+                width: examples.length * cellSize + 'px',
+                height: cellSize + 'px',
+                fontSize: '10px',
+                lineHeight: '10px',
+                background: 'gold',
+              }"
+            >
+              Row {{ seq }}
+            </div>
+          </div>
+        </div>
+        <div style="background: lime; flex: 0 0 40px">Pheno 1</div>
+        <div style="background: green; flex: 0 0 120px">Pheno 2</div>
+        <div style="background: cyan; flex: 0 0 100px">Pheno 3</div>
       </div>
     </div>
   </a-card>
@@ -59,8 +112,17 @@ export default {
     overflow: hidden;
   }
 
-  .content {
+  .header {
+    display: flex;
+  }
+
+  .content-wrapper {
     overflow-y: auto;
+  }
+
+  .content {
+    display: flex;
+    align-items: stretch;
   }
 }
 </style>
--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -5,4 +5,6 @@ export const DEFAULT_HOMOLOGY_ID = parseInt(
   `${import.meta.env.VITE_DEFAULT_HOMOLOGY_ID}`
 )
 export const TITLE = `${import.meta.env.VITE_TITLE}`
+
 export const DEFAULT_SELECTED_REGION: Range = [1, 40]
+export const CELL_SIZE = 10
