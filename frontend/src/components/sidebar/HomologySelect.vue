33f666e5708cb46a815ec0b9b9d75ea44bee867f
Folkert de Vries <github@fdev.nl>
2023-01-25 09:37:10 +0100

    Homology select search

--- frontend/src/components/sidebar/HomologySelect.vue
+++ frontend/src/components/sidebar/HomologySelect.vue
@@ -5,6 +5,11 @@ import { useDataStore } from '@/stores/data'
 import LoadingBox from '@/components/common/LoadingBox.vue'
 import SidebarItem from '@/components/common/SidebarItem.vue'
 
+type Option = {
+  value: number
+  label: string
+}
+
 export default {
   components: {
     LoadingBox,
@@ -14,12 +19,20 @@ export default {
     ...mapState(useDataStore, ['homologies']),
     ...mapWritableState(useDataStore, ['homologyId']),
   },
+  methods: {
+    filterOption(input: string, option: Option) {
+      return option.label.toLowerCase().includes(input.toLowerCase())
+    },
+  },
 }
 </script>
 
 <template>
   <SidebarItem title="Homology">
     <a-select
+      show-search
+      :dropdownMatchSelectWidth="false"
+      :filterOption="filterOption"
       v-model:value="homologyId"
       style="width: 100%"
       v-if="homologies.length !== 0"
@@ -27,6 +40,7 @@ export default {
       <a-select-option
         v-for="item in homologies"
         :value="item.homology_id"
+        :label="item.name"
         v-bind:key="item.homology_id"
       >
         {{ item.name }}
