83037dce76720969dd122e08b1b9bc6b0071ee18
Folkert de Vries <github@fdev.nl>
2023-03-09 16:33:27 +0100

    Add Neo4J connection and example route

--- api/app.py
+++ api/app.py
@@ -1,9 +1,10 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 import os
 import pandas as pd
 from dotenv import load_dotenv
 from flask_json_schema import JsonSchema
+from py2neo import Graph, Node, Relationship
 
 from cluster_functions import (
     create_dendrogram,
@@ -27,6 +28,13 @@ app = Flask(
     static_folder=db_path,
 )
 
+
+# Neo4j graph database.
+graph = Graph(
+    os.environ.get("NEO4J_URI"),
+    user=os.environ.get("NEO4J_USER"),
+    password=os.environ.get("NEO4J_PASSWORD"),
+)
 
 # Load Flask configuration options from environment variables (prefixed with API_).
 app.config.from_prefixed_env("API")
@@ -77,6 +85,22 @@ def get_dendrogram(id):
 
     return create_dendrogram(linkage_matrix, labels)
 
+
+@app.route("/graph/labels")
+def graph_labels():
+    # Use named parameters when using user-provided input to prevent
+    # query injections that a hacker could abuse:
+    # results = graph.run("""
+    #     MATCH (n)
+    #     WHERE person.name = {name}
+    #     RETURN n
+    # """, { "name": request.id })
+
+    results = graph.run("""
+        MATCH (n:sequence)
+        RETURN DISTINCT LABELS(n) AS labels, COUNT(*) AS count
+    """)
+    return jsonify(results.data())
 
 if __name__ == "__main__":
     app.run()
