6352e99b640c547cde620cafce74b2f128fdf0c7
astridvandenbrandt <astridvandenbrandt@gmail.com>
2023-03-21 11:26:42 +0100

    Config ,Gene Set renaming and API

--- api/app.py
+++ api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, Blueprint
 from flask_cors import CORS
 import sys
 import os
@@ -21,7 +21,9 @@ sys.setrecursionlimit(2500)
 load_dotenv(".env")
 
 # Get non-Flask configuration options from environment variables.
-db_path = os.environ.get("API_DB_PATH")
+db_path = os.environ.get("API_DB_PATH_PANVA")
+db_path_pansets = os.environ.get("API_DB_PATH_PANSETS")
+
 
 # Instantiate the app.
 app = Flask(
@@ -32,6 +34,9 @@ app = Flask(
     static_folder=db_path,
 )
 
+# Add PanSets blueprint for extra static folder for other database with static files
+pansets_bp = Blueprint('pansets', __name__, static_url_path="/pansets", static_folder=db_path_pansets)
+app.register_blueprint(pansets_bp)
 
 # Load Flask configuration options from environment variables (prefixed with API_).
 app.config.from_prefixed_env("API")
