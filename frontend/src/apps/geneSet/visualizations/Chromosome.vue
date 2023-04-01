924cf15df00c8824eae37a175bf2a352c7081026
astridvandenbrandt <astridvandenbrandt@gmail.com>
2023-04-01 18:14:51 +0200

    zooming example

--- frontend/src/apps/geneSet/visualizations/Chromosome.vue
+++ frontend/src/apps/geneSet/visualizations/Chromosome.vue
@@ -7,7 +7,7 @@
       sort descending genome
     </button>
   </div>
-
+  <!-- <div><svg id="zoom-example"></svg></div> -->
   <div id="content" style="width: 100%; height: 100%">
     <svg :width="svgWidth" :height="svgHeight">
       <g :id="name"></g>
@@ -2197,6 +2197,65 @@ export default {
     },
   },
   methods: {
+    drawZoomExample() {
+      const width = 500
+      const height = 180
+      const padding = { top: 10, bottom: 50, left: 40, right: 20 }
+
+      const svg = d3
+        .select('#zoom-example')
+        .attr('width', width + padding.right + padding.left)
+        .attr('height', height + padding.top + padding.bottom)
+
+      const plotArea = svg
+        .append('g')
+        .attr('transform', 'translate(' + [padding.left, padding.top] + ')')
+
+      const clippingRect = plotArea
+        .append('clipPath')
+        .attr('id', 'clippy')
+        .append('rect')
+        .attr('width', width)
+        .attr('height', height)
+        .attr('fill', 'none')
+
+      const data = d3.range(100).map(function (d) {
+        return { value: Math.random(), sample: d }
+      })
+
+      const x = d3.scaleLinear().range([0, width]).domain([0, 100])
+      let x2 = x.copy()
+      const y = d3.scaleLinear().range([height, 0]).domain([0, 1])
+
+      const line = d3
+        .line()
+        .x((d) => x2(d.sample))
+        .y((d) => y(d.value))
+
+      const xAxis = d3.axisBottom(x2)
+      const xAxisG = plotArea
+        .append('g')
+        .attr('transform', 'translate(' + [0, height] + ')')
+        .call(xAxis)
+
+      const yAxis = d3.axisLeft(y)
+      const yAxisG = plotArea.append('g').call(yAxis)
+
+      const path = plotArea
+        .append('path')
+        .attr('class', 'zoom')
+        .datum(data)
+        .attr('d', line)
+        .attr('clip-path', 'url(#clippy)')
+
+      const zoom = d3.zoom().on('zoom', function (event) {
+        x2 = event.transform.rescaleX(x)
+        xAxisG.call(xAxis.scale(x2))
+        path.attr('d', line)
+      })
+
+      svg.call(zoom)
+    },
     svg() {
       return d3.select(`#${this.name}`)
     },
@@ -2455,6 +2514,7 @@ export default {
       document.getElementById('content').offsetHeight *
       this.svgHeightScaleFactor
 
+    this.drawZoomExample()
     this.draw()
   },
 }
@@ -2475,5 +2535,11 @@ export default {
 
 .bar-chr:hover {
   fill: $gray-7;
+}
+
+.zoom {
+  fill: none;
+  stroke-width: 1px;
+  stroke: black;
 }
 </style>
